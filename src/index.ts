1a12b0141f1eafe19bb4a0a7caff602afec2bb70
AndreaPontrandolfo <andrea.pontra@gmail.com>
2020-09-18 10:19:58 +0200

    added 2 packages as dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
-  }
-  return a + b;
-};
+export {
+  normalizeString,
+  hasData,
+  getMatches,
+  getTotalOfValues,
+} from 'ramda-addons';
+
+export {
+  capitalize,
+  isServer,
+  sanitizeHTML,
+  getDeviceType,
+  debounce,
+  throttle,
+} from '@dragonsonic/js-utils';
